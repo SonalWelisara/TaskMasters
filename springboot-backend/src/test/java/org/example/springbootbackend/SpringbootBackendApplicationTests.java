af647fda84917325256b535c82ddc6a1c466062a
Sonal Welisara <sonaldilmith15@gmail.com>
2024-04-07 13:16:56 +0530

    intialize

---
+++ springboot-backend/src/test/java/org/example/springbootbackend/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
