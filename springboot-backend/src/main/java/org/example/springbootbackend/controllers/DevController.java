9393150e2a83d6fc8517b74a480c568724031e1e
Sonal Welisara <sonaldilmith15@gmail.com>
2024-04-08 18:46:01 +0530

    Dev Table push

--- springboot-backend/src/main/java/org/example/springbootbackend/controllers/DevController.java
+++ springboot-backend/src/main/java/org/example/springbootbackend/controllers/DevController.java
@@ -1,4 +1,26 @@
 package org.example.springbootbackend.controllers;
 
+import org.example.springbootbackend.services.DevService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class DevController {
+
+    private final DevService devService;
+
+    public DevController(DevService devService) {
+        this.devService = devService;
+    }
+
+    @RequestMapping("/devs")
+    public String getDevs(Model model){
+
+        model.addAttribute("devs", devService.findAll());
+
+        return "devs";
+
+    }
+
 }
--- springboot-backend/src/main/java/org/example/springbootbackend/services/DevServiceImpl.java
+++ springboot-backend/src/main/java/org/example/springbootbackend/services/DevServiceImpl.java
@@ -1,4 +1,20 @@
 package org.example.springbootbackend.services;
 
+import org.example.springbootbackend.domain.Dev;
+import org.example.springbootbackend.repo.DevRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DevServiceImpl implements DevService{
+
+    private final DevRepository devRepository;
+
+    public DevServiceImpl(DevRepository devRepository) {
+        this.devRepository = devRepository;
+    }
+
+    @Override
+    public Iterable<Dev> findAll() {
+        return devRepository.findAll();
+    }
 }
