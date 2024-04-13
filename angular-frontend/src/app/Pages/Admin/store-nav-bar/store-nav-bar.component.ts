7586e114ef7e4cbafa5bb5466476ce27967ce8d6
Ramith Kalhara <kalhara7ramith17@gmail.com>
2024-04-13 12:02:42 +0530

    Store Front end
    create Sore frontend component

---
+++ angular-frontend/src/app/Pages/Admin/store-nav-bar/store-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-nav-bar',
+  templateUrl: './store-nav-bar.component.html',
+  styleUrl: './store-nav-bar.component.css'
+})
+export class StoreNavBarComponent {
+
+}
--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetAllProductComponent } from './Pages/Admin/get-all-product/get-all-product.component';
+import { AddProductComponent } from './Pages/Admin/add-product/add-product.component';
+import { StoreNavBarComponent } from './Pages/Admin/store-nav-bar/store-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetAllProductComponent,
+    AddProductComponent,
+    StoreNavBarComponent
   ],
   imports: [
     BrowserModule,
