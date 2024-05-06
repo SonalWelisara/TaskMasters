c5dba02fce846b32f8f1c4d641013f1c6c538342
Ramith Kalhara <kalhara7ramith17@gmail.com>
2024-05-07 01:43:42 +0530

    StoreMin

--- angular-frontend/src/app/Pages/Customer/user-routing.module.ts
+++ angular-frontend/src/app/Pages/Customer/user-routing.module.ts
@@ -3,33 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { BiddingAdminComponent } from './bidding-admin/bidding-admin.component';
 import { BiddingUpdateComponent } from './bidding-update/bidding-update.component';
 import { BiddingComponent } from './bidding/bidding.component';
-import { HomeAboutComponent } from './home-about/home-about.component';
-import { HomeFooterComponent } from './home-footer/home-footer.component';
-import { HomeServiceComponent } from './home-service/home-service.component';
-import { HomeComponent } from './home/home.component';
+
 import { StoreAddCartComponent } from './store-add-cart/store-add-cart.component';
 import { StoreHomeComponent } from './store-home/store-home.component';
 import { StoreProductDiscriptionComponent } from './store-product-discription/store-product-discription.component';
-import { TempUserComponent } from './temp-user/temp-user.component';
+
 
 const routes: Routes = [
 
-  {
-    path:"",
-    component:TempUserComponent
-  },
-  {
-    path: 'userHome/:id',
-    component: HomeComponent },
-  {
-    path:'homeAbout/:id',
-    component:HomeAboutComponent
-  },
 
-  {
-    path:'homeService/:id',
-    component:HomeServiceComponent
-  },
 
   //change path
   {
@@ -40,10 +22,6 @@ const routes: Routes = [
   {
     path:'storeCart/:id',
     component:StoreAddCartComponent
-  },
-  {
-    path:"homeFooter",
-    component:HomeFooterComponent
   },
   {
     path:"storeProductDis/:id",
--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './pages/Admin/layout/layout.component';
-import { TempUserComponent } from './pages/Customer/temp-user/temp-user.component';
+
 
 //
 import { MemberLayoutComponent } from './modules/core/components/member-layout/member-layout.component';
